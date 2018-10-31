435a18cf257bef4ef4f052271cd561a2e15f7347
Vanessa de La Cuetara <vdelacuetara@gmail.com>
2018-10-31 16:58:47 -0400

    checkpoint address bloc searching attempting to debug error message Failure/Error: __send__(method, file)

--- models/address_book.rb
+++ models/address_book.rb
@@ -10,25 +10,47 @@ require "csv"
 
     def add_entry(name, phone_number, email)
       index = 0
+
       entries.each do |entry|
         if name < entry.name
           break
         end
         index+= 1
       end
+
       entries.insert(index, Entry.new(name, phone_number, email))
     end
 
     def import_from_csv(file_name)
       csv_text = File.read(file_name)
       csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
       csv.each do |row|
         row_hash = row.to_hash
         add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
       end
+
     end
 
     def binary_search(name)
+      lower = 0
+      upper = entries.length - 1
+
+      while lower <= upper
+        mid = (lower + upper) / 2
+        mid_name = entries[mid].name
+
+        if name == mid_name
+          return entries[mid]
+        elsif name < mid_name
+          upper = mid - 1
+        elsif name > mid_name
+          lower = mid + 1
+        end
+        
+      end
+
+      return nil
     end
 
   end
